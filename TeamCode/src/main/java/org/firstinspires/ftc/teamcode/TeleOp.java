586581640c054df03fb1d42783341721441cf491
jgereghty25 <jaygereghty@gmail.com>
2024-01-20 00:41:51 -0500

    cap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -3,6 +3,12 @@ package org.firstinspires.ftc.teamcode;
 //luh TeleOp
 //default position pos = 0.9, vpos = 1
 
+/*flat positions:
+pos = 0 (70 deg), vpos = 0.205, slidePos = 0.299
+pos = 0.05 (72 deg), vpos = 0.21, slidePos = 0.421
+pos = 0.1 (75 deg), vpos = 0.214, slidePos = 0.573
+*/
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -33,13 +39,22 @@ public class TeleOp extends OpMode {
 
     MecanumDrive drive;
 
-    boolean halfSpeedToggle = false;
-    boolean rightBumperLast = false;
+    boolean halfSpeedToggle = true;
+    boolean startLast = false;
 
     boolean drivingReverse = false;
+    boolean shareLast = false;
+
+    boolean clawLOpen = false;
     boolean leftBumperLast = false;
 
+    boolean clawROpen = false;
+    boolean rightBumperLast = false;
+
+    boolean intake = true;
+
     // slidePos is a fraction of the total possible slide extension.
+    double slideLength = 0.0;
     double slidePos = 0.0;
 
     double yMovement;
@@ -56,11 +71,25 @@ public class TeleOp extends OpMode {
     private static double getSlideVelocity(int trigger, double slidePos, double triggerDepth) {
         double velocity = 0.0;
         if (trigger == 1) {
-            velocity = 0.4 * triggerDepth * Math.cos(0.5 * Math.PI * slidePos) + 0.3;
+            velocity = triggerDepth * (0.6 * Math.cos(0.5 * Math.PI * slidePos) + 0.4);
         } else if (trigger == -1) {
-            velocity = -0.4 * triggerDepth * Math.sin(0.5 * Math.PI * slidePos) - 0.3;
+            velocity = triggerDepth * (-0.6 * Math.sin(0.5 * Math.PI * slidePos) - 0.4);
         }
         return(velocity);
+    }
+
+    private static double getSlideAngle(double slideLength) {
+        double angle = Math.toDegrees(Math.acos(22.5 / slideLength)) / 47.8 - 1.4;
+        if (angle >= 0) {
+            return(angle);
+        } else {
+            return(0);
+        }
+    }
+
+    private static double getClawVAngle(double slideLength) {
+        double angle = -Math.toDegrees(Math.asin(22.5 / slideLength)) / 500 + 0.245;
+        return(angle);
     }
 
     public void init() {
@@ -126,72 +155,108 @@ public class TeleOp extends OpMode {
         rotation = gamepad1.right_stick_x;
         drivePower = Math.max(Math.max(Math.abs(yMovement), Math.abs(xMovement)), Math.abs(rotation));
 
-        if (!rightBumperLast && gamepad1.right_bumper) {
+        if (!shareLast && gamepad1.share) {
             halfSpeedToggle = !halfSpeedToggle;
         }
         if (halfSpeedToggle) {
-            drivePower *= 0.5;
+            drivePower *= 0.3;
         }
-        rightBumperLast = gamepad1.right_bumper;
+        shareLast = gamepad1.share;
 
-        slidePos = slide.getCurrentPosition() / 537.7 * 4 * Math.PI / 54.864;
-        if (gamepad1.right_trigger > 0.01) {
-            slidePower = getSlideVelocity(1, slidePos, Math.pow(gamepad1.right_trigger, 2));
-            slide.setPower(slidePower);
-        } else if (gamepad1.left_trigger > 0.01) {
-            slidePower = getSlideVelocity(-1, slidePos, Math.pow(gamepad1.left_trigger, 2));
-            slide.setPower(slidePower);
-        } else {
-            slide.setPower(0);
-        }
-
-        hangLeft.setPower(0);
-        hangRight.setPower(0);
-        if (gamepad1.y) {
-            hangLeft.setPower(-1);
-            hangRight.setPower(-1);
-        }
-        if (gamepad1.a) {
-            hangLeft.setPower(1);
-            hangRight.setPower(1);
-        }
-
-        if (!leftBumperLast && gamepad1.left_bumper) {
+        if (!startLast && gamepad1.start) {
             drivingReverse = !drivingReverse;
         }
         if (drivingReverse) {
             xMovement *= -1;
             yMovement *= -1;
         }
-        leftBumperLast = gamepad1.left_bumper;
+        startLast = gamepad1.start;
 
         /*
+        if (Math.abs(gamepad1.right_stick_y) > 0.01) {
+            pos += Math.pow(gamepad1.right_stick_y, 3) * 0.05;
+        }
+        slideLAngle.setPosition(pos);
+        slideRAngle.setPosition(pos);
+        */
+
+        if (!rightBumperLast && gamepad1.right_bumper) {
+            clawROpen = !clawROpen;
+        }
+        if (clawROpen) {
+            clawR.setPosition(0.5);
+        } else {
+            clawR.setPosition(0);
+        }
+        rightBumperLast = gamepad1.right_bumper;
+
+        if (!leftBumperLast && gamepad1.left_bumper) {
+            clawLOpen = !clawLOpen;
+        }
+        if (clawLOpen) {
+            clawL.setPosition(0.5);
+        } else {
+            clawL.setPosition(0);
+        }
+        leftBumperLast = gamepad1.left_bumper;
+
+        slidePos = slide.getCurrentPosition() / 537.7 * 4 * Math.PI / 54.864;
+        slideLength = slidePos * 54.864 + 38.5;
+        if (gamepad1.right_trigger > 0.01) {
+            slidePower = getSlideVelocity(1, slidePos, Math.pow(gamepad1.right_trigger, 3));
+            slide.setPower(slidePower);
+        } else if (gamepad1.left_trigger > 0.01) {
+            slidePower = getSlideVelocity(-1, slidePos, Math.pow(gamepad1.left_trigger, 3));
+            slide.setPower(slidePower);
+        } else {
+            slide.setPower(0);
+        }
+        pos = getSlideAngle(slideLength);
+        slideLAngle.setPosition(pos);
+        slideRAngle.setPosition(pos);
+        vPos = getClawVAngle(slideLength);
+        clawVAngle.setPosition(vPos);
+
+        if (gamepad1.y) {
+            hangLeft.setPower(-1);
+            hangRight.setPower(-1);
+        } else if (gamepad1.a) {
+            hangLeft.setPower(1);
+            hangRight.setPower(1);
+        } else {
+            hangLeft.setPower(0);
+            hangRight.setPower(0);
+        }
+        /*
         if (gamepad1.dpad_up) {
-            clawVAngle.setPosition(clawVAngle.getPosition() + 0.001);
+            clawVAngle.setPosition(clawVAngle.getPosition() + 0.005);
         }
         if (gamepad1.dpad_down) {
-            clawVAngle.setPosition(clawVAngle.getPosition() - 0.001);
+            clawVAngle.setPosition(clawVAngle.getPosition() - 0.005);
         }
         if (gamepad1.dpad_right) {
-            clawHAngle.setPosition(clawHAngle.getPosition() - 0.001);
+            clawHAngle.setPosition(clawHAngle.getPosition() - 0.005);
         }
         if (gamepad1.dpad_left) {
-            clawHAngle.setPosition(clawHAngle.getPosition() + 0.001);
+            clawHAngle.setPosition(clawHAngle.getPosition() + 0.005);
         }
         */
 
-        clawL.setPosition(lPos);
-        clawR.setPosition(rPos);
+        //*
+        //clawL.setPosition(lPos);
+        //clawR.setPosition(rPos);
         clawHAngle.setPosition(hPos);
         clawVAngle.setPosition(vPos);
-        slideLAngle.setPosition(pos);
-        slideRAngle.setPosition(pos);
+        //slideLAngle.setPosition(pos);
+        //slideRAngle.setPosition(pos);
+        //*/
 
         telemetry.addData("slidePos", slidePos);
         telemetry.addData("halfSpeed", halfSpeedToggle);
         telemetry.addData("drivingReverse", drivingReverse);
         telemetry.addData("clawHAngle", clawHAngle.getPosition());
         telemetry.addData("clawVAngle", clawVAngle.getPosition());
+        telemetry.addData("slideAngle", slideLAngle.getPosition());
         updateTelemetry(telemetry);
 
         drive.moveInTeleop(xMovement, yMovement, rotation, drivePower);
